e54695180b289aa014dd8559caf8e5ff44135552
Marcel Klehr <mklehr@gmx.net>
2013-03-11 20:33:32 +0100

    Refactor ip detection, allow for multiple seeds and fix Node#connected

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 var net  = require('net')
   , Node = require('./lib/node')
+  , Netmask = require('netmask').Netmask
+  , os = require('os')
 
 /**
 Creates a node and binds it to a server
 */
 exports.createNode = function createNode(opts) {
   return new Node(opts)
+}
+
+exports.localIp = function(subnet) {
+  //Determine my IP
+  var interfaces = os.networkInterfaces()
+    , block = new Netmask(subnet) // https://npmjs.org/package/netmask
+    , ip
+  for(var name in interfaces) {
+    if(this.remoteAddress) break
+    for(var i in interfaces[name]) {
+      if(interfaces[name][i].family == 'IPv6') continue
+      if(!block.contains(interfaces[name][i].address)) continue
+      return interfaces[name][i].address
+    }
+  }
+  
+  throw new Error('Couldn\'t determine IP address')
 }
